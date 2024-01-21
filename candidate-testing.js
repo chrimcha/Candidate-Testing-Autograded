9c8146463676a1d172d40266ccf3fc550082c7c6
chrimcha <147195321+chrimcha@users.noreply.github.com>
2024-01-21 17:14:35 -0600

    passed tests 7-10

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,10 +11,9 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
-
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = "";
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -24,17 +23,18 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question);
-
+  for (let i = 0; i < question.length; i++) {
+    console.log(questions[i]);
+  }
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if (candidateAnswer === correctAnswer) {
-    console.log ("Answer is CORRECT!");
+  if (candidateAnswers === correctAnswers) {
+    console.log (`You answer (${candidateAnswers}) is CORRECT, Correct answer: ${correctAnswers}`);
   } else {
-    console.log ("Answer is INCORRECT!")
+    console.log (`You answer (${candidateAnswers}) is INCORRECT, Correct answer: ${correctAnswers}`)
   }
 
 
